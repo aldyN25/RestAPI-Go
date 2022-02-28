9cc158ab7080ea7e9d86c9e5ab77107b8dfe68b9
Aldy Nura <andreasnura25@gmail.com>
2022-02-28 23:01:21 +0700

    Delete Hackday directory

--- Hackday/src/entitas/person.go
+++
@@ -1,9 +0,0 @@
-package entitas
-
-type Person struct {
-	ID        int64  `gorm:"primaryKey;autoIncrement" json:"id" `
-	FirstName string `gorm:"type:varchar(200)" json:"first_name"`
-	LastName  string `gorm:"type:varchar(200)" json:"last_name"`
-	Username  string `gorm:"type:varchar(200)" json:"username"`
-	Password  string `gorm:"type:varchar(200)" json:"password"`
-}
