8dc2d1df45360926479f3286a3a2ab350abea0e0
Aldy Nura <andreasnura25@gmail.com>
2022-08-03 13:46:48 +0700

    Hackday (#1)
    * commit training
    
    * Create README.md
    
    * commit 1 Maret
    
    * comit 1 maret kedua
    
    Co-authored-by: aldyN25 <aldynura25.com>

---
+++ Training/src/controllers/productcontroller/productcontroller.go
@@ -0,0 +1,79 @@
+package productcontroller
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+	"training/src/entitas"
+	"training/src/models"
+)
+
+func Index(response http.ResponseWriter, request *http.Request) {
+	var productModel models.ProductModel
+	products, _ := productModel.FindAll()
+	data := map[string]interface{}{
+		"products": products,
+	}
+
+	tmp, _ := template.ParseFiles("views/product/index.html")
+	tmp.Execute(response, data)
+}
+
+func Add(response http.ResponseWriter, request *http.Request) {
+	tmp, _ := template.ParseFiles("views/product/add.html")
+	tmp.Execute(response, nil)
+}
+
+func ProcessAdd(response http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	var product entitas.Product
+	product.Nama = request.Form.Get("nama")
+	product.Harga, _ = strconv.ParseFloat(request.Form.Get("harga"), 64)
+	product.Jumlah, _ = strconv.ParseInt(request.Form.Get("jumlah"), 10, 64)
+	product.Deskripsi = request.Form.Get("deskripsi")
+	var productModel models.ProductModel
+	productModel.Create(&product)
+	http.Redirect(response, request, "/product", http.StatusSeeOther)
+}
+
+func Delete(response http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	id, _ := strconv.ParseInt(query.Get("id"), 10, 64)
+	var productModel models.ProductModel
+	productModel.Delete(id)
+	http.Redirect(response, request, "/product", http.StatusSeeOther)
+}
+
+func Edit(response http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	id, _ := strconv.ParseInt(query.Get("id"), 10, 64)
+
+	var productModel models.ProductModel
+	product, _ := productModel.Find(id)
+	data := map[string]interface{}{
+		"product": product,
+	}
+	fmt.Println(product)
+	// fmt.Println(product)
+	tmp, _ := template.ParseFiles("views/product/edit.html")
+	tmp.Execute(response, data)
+}
+
+func Update(response http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	var product entitas.Product
+	// query := request.URL.Query()
+	// fmt.Println("id update", query.Get("id"))
+	fmt.Println("id update", request.Form.Get("id"))
+
+	product.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64)
+	product.Nama = request.Form.Get("nama")
+	product.Harga, _ = strconv.ParseFloat(request.Form.Get("harga"), 64)
+	product.Jumlah, _ = strconv.ParseInt(request.Form.Get("jumlah"), 10, 64)
+	product.Deskripsi = request.Form.Get("deskripsi")
+	fmt.Println(product)
+	var productModel models.ProductModel
+	productModel.Update(product)
+	http.Redirect(response, request, "/product", http.StatusSeeOther)
+}
